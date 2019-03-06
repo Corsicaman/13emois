cbf88c78a493828e9445153269238cc026e9f591
Lucas <lucorscia@bell.net>
2019-03-06 02:11:11 -0500

    BG Fix lag

--- app/components/listeDestinations.js
+++ app/components/listeDestinations.js
@@ -14,6 +14,11 @@ const urlJsonDestinations = "/app/BDD.json";
 
 export const listeDestinations = {
     template: `<div id="bodyDestinations">
+            <div id="logo">
+                <router-link :to="{ name: 'accueil'}">
+                    <img src="../images/logos/13EM_Logo_Black_Blue.png" alt="13e LOGO">
+                </router-link>
+            </div>
             <main id="main_lesDestinations" v-if="destinations">
                 <div id="header_liste">
                     <h1 id="titre_liste">LISTE DES DESTINATIONS</h1>
